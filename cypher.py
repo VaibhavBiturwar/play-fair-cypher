0011fc68c411008563c80915dd755cb75b49a68a
Vaibhav Biturwar <vaibhavbiturwar@gmail.com>
2019-08-08 17:56:09 +0530

    final alpha list

--- cypher.py
+++ cypher.py
@@ -2,17 +2,22 @@ import string
 
 alpha = []
 alpha_m = []
+kw = [ _ for _ in "dimat"]
+alpha.append(kw)
+
 z = 0 
 for x in string.ascii_lowercase:
-    if z < 5:
-        if x != 'j':
+    if x not in kw:
+        if z < 5:
+            if x != 'j':
+                alpha_m.append(x)
+                z+=1
+        else:
+            z=1
+            alpha.append(alpha_m)
+            alpha_m=[]
             alpha_m.append(x)
-            z+=1
-    else:
-        z=1
-        alpha.append(alpha_m)
-        alpha_m=[]
-        alpha_m.append(x)
+
 alpha.append(alpha_m)
 
 print(alpha)
